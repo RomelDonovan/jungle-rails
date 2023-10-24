60367af0cf03a5f16b4467dd08d0864f4ae6b92f
RomelDonovan <romeldonovan@live.ca>
2023-10-24 15:45:46 -0400

    Add a test to check for products with cypress

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,5 +1,10 @@
 describe('The Home Page', () => {
-    it('successfully loads', () => {
-      cy.visit('http://localhost:3000')
+  it('successfully loads', () => {
+    cy.visit('http://localhost:3000')
   })
+  
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
 })
