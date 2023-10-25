3c39013c3a6f394e54b381297412ed7b2844a0b6
RomelDonovan <romeldonovan@live.ca>
2023-10-25 13:01:18 -0400

    Add  basic authenticate to dashboard admin controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   def show
     @products_total = Product.count
     @categories_total = Category.count
